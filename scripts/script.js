51bde6c9ca8a2196f8eb3a097a16a17c4acb0974
KDugg84 Duggan <kieron.duggan25@hotmail.com>
2023-08-09 14:45:46 +0000

    Add click function, mouseenter and mouseleave to js file

--- scripts/script.js
+++ scripts/script.js
@@ -5,12 +5,46 @@ $(document).ready(function() {
         $(".stream3").removeClass('highlight_stream');
         $(".stream1").addClass('highlight_stream');
    });
+
    $("#stream2_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream2").addClass('highlight_stream');
    });
+
+   $("#stream3_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream3").addClass('highlight_stream');
+   });
+}); 
+
+// Another method of using the above:
+
+$(document).ready(function() {
+    $("#stream1_btn").click(function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream1").addClass('highlight_stream');
+   });
+// We also trigger events when the mouse is hovering:
+   $("#stream1_btn").mouseenter/mouseleave(function() {
+    $(".stream1").removeClass('highlight_stream');
+    $(".stream2").removeClass('highlight_stream');
+    $(".stream3").removeClass('highlight_stream');
+    $(".stream1").addClass('highlight_stream');
+});
+
+   $("#stream2_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream2").addClass('highlight_stream');
+   });
+
    $("#stream3_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
