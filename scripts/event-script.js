5f2c0530f3ed5300a62750b939aab560a2314e0e
KDugg84 Duggan <kieron.duggan25@hotmail.com>
2023-08-09 16:53:46 +0000

    Add code to method chaining js file

---
+++ scripts/event-script.js
@@ -0,0 +1,99 @@
+// $(document).ready(function() {
+//     $("#stream1_btn").on("click", function() {
+//         $(".stream1").removeClass('highlight_stream');
+//         $(".stream2").removeClass('highlight_stream');
+//         $(".stream3").removeClass('highlight_stream');
+//         $(".stream1").addClass('highlight_stream');
+//    });
+
+//    $("#stream2_btn").on("click", function() {
+//         $(".stream1").removeClass('highlight_stream');
+//         $(".stream2").removeClass('highlight_stream');
+//         $(".stream3").removeClass('highlight_stream');
+//         $(".stream2").addClass('highlight_stream');
+//    });
+
+//    $("#stream3_btn").on("click", function() {
+//         $(".stream1").removeClass('highlight_stream');
+//         $(".stream2").removeClass('highlight_stream');
+//         $(".stream3").removeClass('highlight_stream');
+//         $(".stream3").addClass('highlight_stream');
+//    });
+// }); 
+
+// // Another method of using the above
+
+// $(document).ready(function() {
+//     $("#stream1_btn").click(function() {
+//         $(".stream1").removeClass('highlight_stream');
+//         $(".stream2").removeClass('highlight_stream');
+//         $(".stream3").removeClass('highlight_stream');
+//         $(".stream1").addClass('highlight_stream');
+//    });
+// // We also trigger events when the mouse is hovering
+
+//    $("#stream1_btn").mouseenter/mouseleave(function() {
+//     $(".stream1").removeClass('highlight_stream');
+//     $(".stream2").removeClass('highlight_stream');
+//     $(".stream3").removeClass('highlight_stream');
+//     $(".stream1").addClass('highlight_stream');
+// });
+
+// }); 
+
+// //Animations with jQuery
+
+// $(document).ready(function() {
+//     $("#stream1_btn").on("click", function() {
+//         $(".stream1").show();
+//         $(".stream1").show('slow');
+//         $(".stream1").show('medium');
+//         $(".stream1").show('fast');
+//         //Speed in milliseconds
+//         $(".stream1").show(1000);
+//    });
+
+//    $("#stream1_btn").on("click", function() {
+//     $(".stream1").hide();
+//     $(".stream1").hide('slow');
+//     $(".stream1").hide('medium');
+//     $(".stream1").hide('fast');
+//     $(".stream1").hide(1000);
+// });
+
+// $("#stream1_btn").on("click", function() {
+//     $(".stream1").slideDown();
+//     $(".stream1").slideDown(1000);
+// });
+
+// $("#stream1_btn").on("click", function() {
+//     $(".stream1").slideUp();
+//     $(".stream1").slideUp(1000);
+// });
+
+// $("#stream1_btn").on("click", function() {
+//     $(".stream1").slideToggle();
+//     $(".stream1").slideToggle(1000);
+// });
+
+// $("#stream1_btn").on("click", function() {
+//     $(".stream1").fadeIn();
+//     $(".stream1").fadeIn(1000);
+// });
+
+// $("#stream1_btn").on("click", function() {
+//     $(".stream1").fadeOut();
+//     $(".stream1").fadeOut(1000);
+// });
+
+// $("#stream1_btn").on("click", function() {
+//     $(".stream1").fadeToggle();
+//     $(".stream1").fadeToggle(1000);
+// });
+
+// $("#stream1_btn").on("click", function() {
+//     //This takes two parameters, speed and opacity
+//     $(".stream1").fadeTo(1000, 0.5);
+// });
+
+// }); 
